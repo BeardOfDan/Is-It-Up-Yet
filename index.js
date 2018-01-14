a70aad24b663cb68dff79922d297c5d603a32d74
Daniel Roberts <BeardOfDan@gmail.com>
2018-01-13 18:39:29 -0800

    (feat) Add a public directory

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+app.use(express.static(__dirname + '/public/'));
 
 app.listen(PORT, () => {
   console.log('Listening on port: ' + PORT);
