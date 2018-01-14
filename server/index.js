8422e79fa8b9d4e176abf6a540cf9cd2bad43d7c
Daniel Roberts <BeardOfDan@gmail.com>
2018-01-13 19:08:54 -0800

    (feat) Add capacity for local environment variables

--- server/index.js
+++ server/index.js
@@ -2,16 +2,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(path.join(__dirname + '/public/')));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
+
+// get environment variables
+require('dotenv').config();
+
+
 
 
 
